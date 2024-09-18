1d204702ede273f9504fa92d5b757828969d4f1a
gomgoon <147135421+gomgoon@users.noreply.github.com>
2024-09-18 14:00:00 +0900

    [Silver III] Title: N과 M (3), Time: 456 ms, Memory: 71188 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,52 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static boolean[] visit;
+    public static int[] arr;
+    public static int N,M;
+    public static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // 정적변수 N과 M을 초기화해준다.
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        visit = new boolean[N];
+        arr = new int[M];
+
+        dfs(0);
+        System.out.println(sb);
+    }
+
+    public static void dfs(int depth) {
+        if (depth == M) {
+            for (int val : arr) {
+                sb.append(val).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            /*if (!visit[i]) {
+                visit[i] = true;
+                arr[depth] = i + 1;
+                dfs(n, m, depth + 1);
+
+                visit[i] = false;
+            }*/
+            arr[depth] = i + 1;
+            dfs(depth + 1);
+        }
+    }
+
+}
