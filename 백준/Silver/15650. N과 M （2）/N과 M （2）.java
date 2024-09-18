964f1bc6e8b4e1e8a27edd3b2f1bb04aad315809
gomgoon <147135421+gomgoon@users.noreply.github.com>
2024-09-18 13:52:02 +0900

    [Silver III] Title: N과 M (2), Time: 184 ms, Memory: 18308 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class Main {
+    public static boolean[] visit;
+    public static int[] arr;
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N, M;
+
+        N = scanner.nextInt();
+        M = scanner.nextInt();
+        visit = new boolean[N];
+        arr = new int[M];
+
+        dfs(N, M, 0);
+    }
+
+    public static void dfs(int n, int m, int depth) {
+        if (depth == m) {
+            for (int val : arr) {
+                System.out.print(val + " ");
+            }
+            System.out.println();
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (!visit[i]) {
+                if ((depth == 0)||(depth > 0 && arr[depth - 1] < (i + 1))) {
+                    visit[i] = true;
+                    arr[depth] = i + 1;
+                    dfs(n, m, depth + 1);
+
+                    visit[i] = false;
+                }
+            }
+        }
+    }
+
+}
