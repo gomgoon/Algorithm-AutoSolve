a7f28451358b84b57ac8f47814c089dc39d40a10
gomgoon <147135421+gomgoon@users.noreply.github.com>
2024-11-20 11:24:29 +0900

    [Silver IV] Title: 큐, Time: 432 ms, Memory: 116404 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,32 @@
+from collections import deque
+
+deq_arr = deque()
+
+n = int(input())
+
+for _ in range(n):
+    instruction = input().split()
+    if instruction[0] == 'push':
+        deq_arr.append(int(instruction[1]))
+    elif instruction[0] == 'pop':
+        if deq_arr:
+            print(deq_arr.popleft())
+        else:
+            print(-1)
+    elif instruction[0] == 'size':
+        print(len(deq_arr))
+    elif instruction[0] == 'empty':
+        if deq_arr:
+            print(0)
+        else:
+            print(1)
+    elif instruction[0] == 'front':
+        if deq_arr:
+            print(deq_arr[0])
+        else:
+            print(-1)
+    elif instruction[0] == 'back':
+        if deq_arr:
+            print(deq_arr[-1])
+        else:
+            print(-1)
