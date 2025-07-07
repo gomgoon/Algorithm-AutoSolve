9ddc5541c28bd0f8baa7887f941e3e73313748ea
gomgoon <147135421+gomgoon@users.noreply.github.com>
2025-07-07 16:57:38 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 288 ms, Memory: 152448 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.py
@@ -1,14 +1,19 @@
-s = input()
+import sys
+input = sys.stdin.readline
+print = sys.stdout.write
+
+s = input().strip()
 q = int(input())
+prefix = [[0] * (len(s) + 1) for _ in range(26)]
+
+for i in range(len(s)):
+    idx = ord(s[i]) - ord('a')
+    for j in range(26):
+        prefix[j][i + 1] = prefix[j][i]
+    prefix[idx][i + 1] += 1
 
 for _ in range(q):
-    a,l,r = input().split()
+    a, l, r = input().split()
     l, r = int(l), int(r)
-    sub = s[l:r + 1]
-    res = 0
-    
-    for i in range(len(sub)):
-        if sub[i] == a:
-            res += 1
-
-    print(res)
+    n = ord(a) - ord('a')
+    print(str(prefix[n][r + 1] - prefix[n][l]) + '\n')
